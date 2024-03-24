dc213a07136c416199a794057ba3ecad8f3dbe6a
Matteo DE VITA <matteo.devita7@gmail.com>
2024-03-24 13:00:56 +0100

    Add logger lib. There is still some improvements to be made

---
+++ lib/logger/include/public/logger/logger.hpp
@@ -0,0 +1,66 @@
+#ifndef LIMINAL_LIB_LOGGER_HPP
+#define LIMINAL_LIB_LOGGER_HPP
+
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <memory>
+
+namespace liminal {
+
+    namespace logger {
+
+        namespace _private {
+            enum class _Color;
+        }
+
+        enum class Level {
+            TRACE,
+            DEBUG,
+            INFO,
+            WARNING,
+            ERROR,
+            FATAL
+        };
+
+        void setLevel(const Level &level);
+
+        class Logger {
+            private:
+                class _LoggerImpl;
+                std::unique_ptr<_LoggerImpl> _loggerImpl;
+                
+            public:
+                Logger(
+                    std::ostream &stream,
+                    const Level &level,
+                    const _private::_Color &color
+                );
+                ~Logger() = default;
+
+                template<typename T>
+                Logger &operator<<(const T &message);
+
+                //MAX IS 2048
+                template<size_t N>
+                Logger &operator<<(const char (&message)[N]);
+
+                Logger &operator<<(std::ostream& (*manipulator)(std::ostream&));
+        };        
+        
+        // Use those instances
+        extern Logger trace;
+        extern Logger debug;
+        extern Logger info;
+        extern Logger warn;
+        extern Logger error;
+        extern Logger fatal;
+
+    } // namespace logger
+    
+
+} // namespace liminal
+
+#endif // LIMINAL_LIB_LOGGER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -27,43 +27,47 @@
 
 #include "liminal_string_extras/string.hpp"
 
+#include "logger/logger.hpp"
+
 #include <iostream>
 #include <typeinfo>
 #include <cxxabi.h>
 #include <string>
 
 int main() {
-    vulkan_wrapper::VulkanHdl handler;
-    liminal_fs::InFile file("./README.md");
-    file.open();
-    file.read();
-    file.close();
-    std::cout << file.get_content() << std::endl;
+    liminal::logger::debug << "je debug" << "" << '7' << 45*89 << std::endl;
+    liminal::logger::trace << "je trace" << std::endl;
+    // vulkan_wrapper::VulkanHdl handler;
+    // liminal_fs::InFile file("./README.md");
+    // file.open();
+    // file.read();
+    // file.close();
+    // std::cout << file.get_content() << std::endl;
     
-    liminal_json_io::InJson json;
+    // liminal_json_io::InJson json;
 
-    json.parse("./tests/assets/bitNestedNoErrors.json");
+    // json.parse("./tests/assets/bitNestedNoErrors.json");
 
-    liminal_json_io::types::Object_t object{json.get<liminal_json_io::types::Object_t>("address").value()};
+    // liminal_json_io::types::Object_t object{json.get<liminal_json_io::types::Object_t>("address").value()};
     
     
-    std::cout << json.get<liminal_json_io::types::Object_t>("address").value().get<liminal_json_io::types::FloatNum_t>("anotherData").value() << std::endl;
-    std::cout << json.get<liminal_json_io::types::Array_t>("hobbies").value().get<liminal_json_io::types::String_t>(1).value() << std::endl;
-    std::cout << json.get<liminal_json_io::types::FloatNum_t>("address.anotherData").value() << std::endl;
-    std::cout << json.get<liminal_json_io::types::IntNum_t>("address.story.left").value() << std::endl;
-    std::cout << json.get<liminal_json_io::types::String_t>("hobbies[4].secondNestedHobbies[1]").value() << std::endl;
-    liminal_json_io::types::Object_t mainObject = json.get<liminal_json_io::types::Object_t>().value();
+    // std::cout << json.get<liminal_json_io::types::Object_t>("address").value().get<liminal_json_io::types::FloatNum_t>("anotherData").value() << std::endl;
+    // std::cout << json.get<liminal_json_io::types::Array_t>("hobbies").value().get<liminal_json_io::types::String_t>(1).value() << std::endl;
+    // std::cout << json.get<liminal_json_io::types::FloatNum_t>("address.anotherData").value() << std::endl;
+    // std::cout << json.get<liminal_json_io::types::IntNum_t>("address.story.left").value() << std::endl;
+    // std::cout << json.get<liminal_json_io::types::String_t>("hobbies[4].secondNestedHobbies[1]").value() << std::endl;
+    // liminal_json_io::types::Object_t mainObject = json.get<liminal_json_io::types::Object_t>().value();
 
-    // std::cout << json.getType() << std::endl;
-    // std::cout << json.getType("address") << std::endl;
-    // std::cout << json.getType("address.anotherData") << std::endl;
-    // std::cout << json.getType("address.story.left") << std::endl;
-    // std::cout << json.getType("hobbies[4].secondNestedHobbies[1]") << std::endl;
+    // // std::cout << json.getType() << std::endl;
+    // // std::cout << json.getType("address") << std::endl;
+    // // std::cout << json.getType("address.anotherData") << std::endl;
+    // // std::cout << json.getType("address.story.left") << std::endl;
+    // // std::cout << json.getType("hobbies[4].secondNestedHobbies[1]") << std::endl;
     
-    while (handler.window.shouldClose() == false) {
-        handler.window.pollEvents();
-        handler.drawFrame();
-    }
-    handler.waitIdle();
+    // while (handler.window.shouldClose() == false) {
+    //     handler.window.pollEvents();
+    //     handler.drawFrame();
+    // }
+    // handler.waitIdle();
     return  EXIT_SUCCESS;
 }
